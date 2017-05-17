a9886d3fdbebbc623657ca2c00b1eb0b43d8b8bd
jmcgraw94 <jmcgraw@purdue.edu>
2017-05-17 18:26:49 -0400

    [Stable] First Commit

---
+++ EditorToolKit_Editor.cs
@@ -0,0 +1,23 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public class EditorToolKit_Editor : ScriptableObject {
+    [MenuItem("Utilities/Deselect All &#d")]
+    static void DoDeselect() {
+        Selection.objects = new UnityEngine.Object[0];
+    }
+
+    [MenuItem("Utilities/Snap Down &#s")]
+    static void SnapToGround() {
+        RaycastHit Hit;
+
+        foreach (GameObject CurSel in Selection.objects)
+            if (Physics.Raycast(CurSel.transform.position, Vector3.down, out Hit)) {
+                if (Hit.collider != null) {
+					CurSel.transform.position = Hit.point;
+                }
+            }
+    }
+}
+#endif
