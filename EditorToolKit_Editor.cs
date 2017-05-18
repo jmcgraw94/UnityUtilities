db435c397a7b661867dc67c6a24481029ed85354
jmcgraw94 <jmcgraw@purdue.edu>
2017-05-17 21:20:05 -0400

    [Stable] Added Comments + Extra Utility Classes

--- EditorToolKit_Editor.cs
+++ EditorToolKit_Editor.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 //Shortcut Commands: https://docs.unity3d.com/ScriptReference/MenuItem.html
 
 public class EditorToolKit_Editor : ScriptableObject {
+	
     [MenuItem("Utilities/Deselect All &#d")]
     static void DoDeselect() {
         Selection.objects = new UnityEngine.Object[0];
@@ -27,6 +28,75 @@ public class EditorToolKit_Editor : ScriptableObject {
         foreach (GameObject CurSel in Selection.objects) {
             CurSel.SetActive(!CurSel.activeSelf);
         }
+    }
+}
+
+[ExecuteInEditMode]
+public class RecursiveMaterialApply_Editor : MonoBehaviour {
+//Apply a material to all children of this script's gameobject
+
+    public Material Material;
+    public bool Apply = false;
+
+    void Update() {
+        if (Apply) {
+            Apply = false;
+            foreach (MeshRenderer MR in GetComponentsInChildren<MeshRenderer>()) {
+                MR.material = Material;
+            }
+        }
+    }
+}
+
+[ExecuteInEditMode]
+public class FindAndReplace_Editor : MonoBehaviour {
+//Perform a naming find and replace operation on the hierarchy
+
+    public string Find = "";
+    public string ReplaceWith = "";
+
+    string LastFind = "";
+    string LastReplace = "";
+
+    [Header("Find and Replace")]
+    public bool Replace = false;
+    public bool Undo = false;
+
+    void Start() {
+
+    }
+
+    void FindAndReplace(string f, string r, bool UpdateLast = true) {
+        Replace = false;
+
+        if (Find.ToLower().Equals(ReplaceWith.ToLower())) {
+            Debug.LogWarning("Rename: No Change");
+            return;
+        }
+
+        if (UpdateLast) {
+            LastFind = f;
+            LastReplace = r;
+        }
+
+        foreach (GameObject G in FindObjectsOfType<GameObject>()) {
+            if (G.name.Contains(f)) {
+                G.name = G.name.Replace(f, r);
+            }
+        }
+    }
+
+
+    void Update() {
+
+        if (Undo) {
+            Undo = false;
+
+            FindAndReplace(LastReplace, LastFind, false);
+        }
+
+        if (Replace)
+            FindAndReplace(Find, ReplaceWith);
     }
 }
 #endif
